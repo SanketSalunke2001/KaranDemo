509acb2c0ef980a11ceb19af72202497c6b90bc0
SanketSalunke2001 <salunkesanket49@gmail.com>
2025-11-14 22:12:49 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className="p-[15px] shadow-2xl h-[60px] flex items-center">
+            <div className="max-w-[1200px] mx-auto h-full flex items-center border-2 border-red-500 w-full px-4">
+
+
+
+            </div>
+        </header>
+    )
+}
+
